b1693348c0b806bf85c7e89fcfa193d58e81b4c8
Laryn Kragt Bakker <laryn@kragtbakker.com>
2023-08-26 18:24:00 -0500

    Issue #21: Content edit sidebar layout. Follow up to attach action buttons to form after moving them around.

--- dist/js/edit_form.js
+++ dist/js/edit_form.js
@@ -2,6 +2,6 @@ var $;
 
 $ = jQuery, Backdrop.behaviors.ginEditForm = {
   attach: function(context, settings) {
-    $(".block-page-title-block").append($(".form-actions"));
+    $(".block-page-title-block").append($(".form-actions")), $(".form-actions input").attr("form", "page-node-form");
   }
 };
--- js/edit_form.js
+++ js/edit_form.js
@@ -7,6 +7,7 @@
   Backdrop.behaviors.ginEditForm = {
     attach: function (context, settings) {
       $('.block-page-title-block').append($('.form-actions'));
+      $('.form-actions input').attr('form', 'page-node-form');
     }
   };
 
