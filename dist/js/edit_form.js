ad199ff327ac5c2267ce3ad62224c99fc42d9353
Laryn Kragt Bakker <laryn@kragtbakker.com>
2023-09-02 23:07:59 -0500

    Issue #54: Use a jQuery approach to node edit sidebar.

--- dist/js/edit_form.js
+++ dist/js/edit_form.js
@@ -1,7 +1,11 @@
-(Backdrop => {
-  Backdrop.behaviors.ginEditForm = {
-    attach: function(context, settings) {
-      $(".block-page-title-block").append($(".form-actions")), $(".form-actions input").attr("form", $(".node-form").attr("id"));
-    }
-  };
-})(Backdrop);
+var $;
+
+$ = jQuery, Backdrop.behaviors.ginEditForm = {
+  attach: function(context, settings) {
+    $(document).ready((function() {
+      $(".node-form > div").addClass("layout-region-node-main"), $(".node-form").append('<div class="layout-region-node-secondary"></div>'), 
+      $(".layout-region-node-secondary").append($(".node-edit-settings")), $(".block-page-title-block").append($(".form-actions")), 
+      $(".form-actions input").attr("form", $(".node-form").attr("id"));
+    }));
+  }
+};
--- js/edit_form.js
+++ js/edit_form.js
@@ -1,13 +1,20 @@
-((Backdrop) => {
+(function ($) {
   /**
-   * Move action buttons to the sticky area.
+   * Move action buttons to the sticky area, vertical tabs into the sidebar.
    */
 
   Backdrop.behaviors.ginEditForm = {
     attach: function (context, settings) {
-      $('.block-page-title-block').append($('.form-actions'));
-      $('.form-actions input').attr('form', $('.node-form').attr('id'));
+      $(document).ready(function () {
+        $('.node-form > div').addClass('layout-region-node-main');
+        $('.node-form').append('<div class="layout-region-node-secondary"></div>');
+        $('.layout-region-node-secondary').append($('.node-edit-settings'));
+        // $('.layout-region-node-secondary .vertical-tabs-pane').addClass('collapsible');
+        // $('.layout-region-node-secondary .vertical-tabs-pane').removeClass('vertical-tabs-pane');
+        $('.block-page-title-block').append($('.form-actions'));
+        $('.form-actions input').attr('form', $('.node-form').attr('id'));
+      });
     }
   };
 
-})(Backdrop);
+})(jQuery);
