0ed05d7659372e3866417a658359e8abc430be0a
laryn <laryn@kragtbakker.com>
2023-12-06 12:33:20 -0600

    Improve targeting of sidebar edit js.

--- dist/js/edit_form.js
+++ dist/js/edit_form.js
@@ -2,12 +2,13 @@ var $;
 
 $ = jQuery, Backdrop.behaviors.ginEditForm = {
   attach: function(context, settings) {
+    const hideLabel = Backdrop.t("Hide sidebar panel");
     $(".node-form").once("gin-edit-form").each((function() {
-      $("> div", this).addClass("layout-region-node-main"), $(this).append('<div id="gin_sidebar" class="layout-region-node-secondary"></div>'), 
-      $(".layout-region-node-secondary").append($(".node-edit-settings")), $(".block-page-title-block").append($(".form-actions"));
-      const hideLabel = Backdrop.t("Hide sidebar panel");
-      $(".form-actions").prepend('<a href="#toggle-sidebar" class="meta-sidebar__trigger trigger" role="button" title="' + hideLabel + '" aria-controls="gin_sidebar"><span class="visually-hidden">' + hideLabel + "</span></a>"), 
-      $(".form-actions input").attr("form", $(this).attr("id"));
+      $("> div:first-child", this).addClass("layout-region-node-main"), $("> .layout-region-node-main > .form-actions", this).prepend('<a href="#toggle-sidebar" class="meta-sidebar__trigger trigger" role="button" title="' + hideLabel + '" aria-controls="gin_sidebar"><span class="visually-hidden">' + hideLabel + "</span></a>"), 
+      $("> .layout-region-node-main > .form-actions input", this).attr("form", $(this).attr("id")), 
+      $(".block-page-title-block").append($("> .layout-region-node-main > .form-actions", this)), 
+      $(this).append('<div id="gin_sidebar" class="layout-region-node-secondary"></div>'), 
+      $(".layout-region-node-secondary", this).append($(".node-edit-settings"));
     }));
   }
 };
